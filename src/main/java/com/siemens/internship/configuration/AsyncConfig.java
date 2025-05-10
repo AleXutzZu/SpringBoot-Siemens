385d385cd42b7c85b6939b76394ee9498251d5d0
AleXutzZu <davidalexrobertyt@gmail.com>
2025-05-11 00:56:00 +0300

    Add Spring managed executor for async operations

---
+++ src/main/java/com/siemens/internship/configuration/AsyncConfig.java
@@ -0,0 +1,18 @@
+package com.siemens.internship.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean
+    public Executor taskExecutor() {
+        return Executors.newFixedThreadPool(10);
+    }
+}
