44bedd8b29df41564f8df8ad5bc918cca78c5131
AleXutzZu <davidalexrobertyt@gmail.com>
2025-05-11 00:56:23 +0300

    Add error catch-all handler for internal server errors

--- src/main/java/com/siemens/internship/advice/ItemControllerAdvice.java
+++ src/main/java/com/siemens/internship/advice/ItemControllerAdvice.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.concurrent.CompletionException;
+
 @RestControllerAdvice(assignableTypes = ItemController.class)
 public class ItemControllerAdvice {
 
@@ -13,4 +15,13 @@ public class ItemControllerAdvice {
     public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException exception) {
         return ResponseEntity.badRequest().body(exception.getMessage());
     }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleExceptions(Exception ex) {
+        //Added error handling for the /api/items/process route
+        Throwable cause = (ex instanceof CompletionException) ? ex.getCause() : ex;
+        return ResponseEntity.internalServerError()
+                .body("Error during processing: " + cause.getMessage());
+    }
+
 }
